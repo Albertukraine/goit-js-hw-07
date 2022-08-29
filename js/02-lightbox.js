ed097a9585771a2f7b74ad1c3d17de2e68ee52a3
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-08-29 14:05:45 +0300

    resolve 2nd task but have bugs

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryTabsContainer = galleryItems.map((image) => {
+  return ` <a class = "gallery__item" href = "${image.original}">
+<img class = "gallery__image" alt = "${image.description}" src = "${image.preview}"></a>`;
+});
+
+galleryEl.addEventListener("click", (event) => {
+
+    let lightbox = new SimpleLightbox('.gallery a');
+      event.preventDefault();
+    
+    });
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryTabsContainer.join(""));
+
+
+
+
+
+
